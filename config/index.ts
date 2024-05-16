e2a5a9e4824c4ec0ea3881ef9c429e4fb6690b6d
zakaria <zakariabaouali@outlook.com>
2024-05-16 23:32:17 +0100

    update config file

--- config/index.ts
+++ config/index.ts
@@ -4,6 +4,6 @@ const MONGODB_DB_NAME = process.env.MONGODB_DB_NAME;
 const MONGODB_IP = process.env.MONGODB_IP;
 const MONGODB_PORT = process.env.MONGODB_PORT;
 
-export const SERVER_PORT = process.env.PORT;
+export const SERVER_PORT = process.env.PORT || 8080;
 export const MONGODB_URI = `mongodb://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_IP}:${MONGODB_PORT}/${MONGODB_DB_NAME}`;
 export const BASE_ROUTE = "/api/v1";
