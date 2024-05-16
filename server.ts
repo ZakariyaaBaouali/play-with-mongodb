50a148908d7f75ee0510c07b6a942f108a47f5f5
zakaria <zakariabaouali@outlook.com>
2024-05-16 23:32:56 +0100

    complete server setup

--- server.ts
+++ server.ts
@@ -1,17 +1,30 @@
 import express, { Request, Response, urlencoded } from "express";
 import { BASE_ROUTE, SERVER_PORT } from "./config";
 import { ConnectToDB } from "./db";
+import movieController from "./controllers/Movie.controller";
 
 const app = express();
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
+app.disable("X-Powered-By");
 
-app.use(`${BASE_ROUTE}`, (req: Request, res: Response) => {
+app.get(`${BASE_ROUTE}`, (req: Request, res: Response) => {
   return res.status(200).send("🧑‍🚀Server working...!");
 });
 
-ConnectToDB();
+//movies handler
+app.use(`${BASE_ROUTE}/movies`, movieController);
+
+//ConnectToDB();
 
 app.listen(SERVER_PORT, () =>
   console.log(`Server running on port ${SERVER_PORT} 🧑‍🚀`)
 );
+
+declare global {
+  namespace Express {
+    interface Request {
+      thumb?: string;
+    }
+  }
+}
